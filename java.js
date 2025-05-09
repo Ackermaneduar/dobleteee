00a85757ec3f4d08fdabd9aa27a4a6d31529e472
Ackermaneduar <114102167+Ackermaneduar@users.noreply.github.com>
2025-05-09 02:13:55 -0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const title = document.querySelector('.title');
+    title.addEventListener('click', () => {
+        alert('¡Vamos por el doblete!');
+    });
+});
